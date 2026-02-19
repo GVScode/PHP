e578fd9d1d8ed99e826cf66b697d8f4289d65430
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-02-19 15:30:01 -0300

    Ajuste index

--- estudos/decisao.php
+++ estudos/decisao.php
@@ -4,16 +4,15 @@ $idade = 16;
 $numeroDePessoas = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou";
-echo "a partir de 16 anos acompanhado" . PHP_EOL;
+echo " a partir de 16 anos acompanhado(a)" . PHP_EOL;
 
 if ($idade >=18){
     echo "você tem $idade anos, pode entrar sozinho!";
-}else {
-    if ($idade >= 16 && $numeroDePessoas >1){
-    echo "você tem $idade anos, e está acompanhado, pode entrar. ";
+}elseif ($idade >= 16 && $numeroDePessoas > 1){
+    echo "você tem $idade anos, e está acompanhado(a), pode entrar. ";
 }else{
     echo "você só tem $idade anos, e está sozinho, sai quente!";
-     }
+     echo PHP_EOL . "É uma pena";
 }
 echo PHP_EOL;
 echo "Adeus!";
