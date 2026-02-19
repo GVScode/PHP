b05f2f9186daf0aa3c61304f61e5622555605c44
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-02-19 12:24:32 -0300

    ajuste na estrutura de decisao

--- estudos/decisao.php
+++ estudos/decisao.php
@@ -1,15 +1,19 @@
 <?php
 
-$idade = 18;
-$numeroDePessoas = 2;
+$idade = 16;
+$numeroDePessoas = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou";
-echo "a partir de 16 anos acompanhado";
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
 if ($idade >=18){
     echo "você tem $idade anos, pode entrar sozinho!";
+}else {
+    if ($idade >= 16 && $numeroDePessoas >1){
+    echo "você tem $idade anos, e está acompanhado, pode entrar. ";
 }else{
-    echo "você tem $idade anos, não pode entrar sozinho. ";
+    echo "você só tem $idade anos, e está sozinho, sai quente!";
+     }
 }
 echo PHP_EOL;
 echo "Adeus!";
