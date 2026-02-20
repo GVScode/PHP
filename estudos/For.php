53b54123b9c77670172f3bb1d9b1f1d6e5dce110
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-02-20 15:35:07 -0300

    alteraçao arquivo for.php

--- estudos/For.php
+++ estudos/For.php
@@ -1,10 +1,30 @@
 <?php
-
-$maior = 0;
-$menor = 0;
+$maior = PHP_INT_MIN;  // Inicializa com o menor valor possível
+$menor = PHP_INT_MAX;  // Inicializa com o maior valor possível
 $posmaior = 0;
 $posmenor = 0;
+$vetor = array();      // Declara o array corretamente
 
-for ($i=0; $i<15; $i++){
-echo "Digite o ". ($i + 1). " numero: ";
+// Lê 15 números e armazena no vetor
+for ($i = 0; $i < 15; $i++) {
+    $vetor[$i] = (float) readline("Digite o " . ($i + 1) . "º número: ");
 }
+
+// Encontra o maior número e sua posição
+for ($i = 0; $i < 15; $i++) {
+    if ($vetor[$i] > $maior) {
+        $maior = $vetor[$i];
+        $posmaior = $i + 1;  // Posição começa em 1
+    }
+}
+
+// Encontra o menor número e sua posição
+for ($i = 0; $i < 15; $i++) {
+    if ($vetor[$i] < $menor) {
+        $menor = $vetor[$i];
+        $posmenor = $i + 1;  // Posição começa em 1
+    }
+}
+echo "O maior número é: $maior e se encontra na posição $posmaior" . PHP_EOL;
+echo "O menor número é: $menor e se encontra na posição $posmenor" . PHP_EOL;
+?>
