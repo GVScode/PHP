0bd391e02ee952f1f465507f0fef2b11f9f87e41
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-02-20 08:08:09 -0300

    for, do while, while

---
+++ estudos/For.php
@@ -0,0 +1,10 @@
+<?php
+
+$maior = 0;
+$menor = 0;
+$posmaior = 0;
+$posmenor = 0;
+
+for ($i=0; $i<15; $i++){
+echo "Digite o ". ($i + 1). " numero: ";
+}
---
+++ estudos/do_while.php
@@ -0,0 +1,11 @@
+<?php
+
+$qtdnumero = 0;
+$nome = readline("Digite seu nome: ");
+echo "boa tarde $nome, seja muito bem vindo ao meu sistema:" . PHP_EOL;
+
+do {
+echo "O numero da vez é: $qtdnumero" . PHP_EOL;
+
+$qtdnumero ++;
+}while($qtdnumero < 15);
