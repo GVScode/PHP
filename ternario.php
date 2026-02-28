d4e999028401671097cc2cddb30dd104ab31c477
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-02-28 07:13:48 -0300

    ok?

--- ternario.php
+++ ternario.php
@@ -1,5 +1,5 @@
 <?php
 
 $idade = 18;
-$mensagem = $idade < 18 ? 'Você é menor de idade não pode entrar' : 'Você é maior de idade, pode entrar';
+$mensagem = $idade < 18 ? 'Você é menor de idade não pode entrar ok?' : 'Você é maior de idade, pode entrar ok?';
 echo $mensagem;
