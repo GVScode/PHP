51da4c76b19bcaad6672ac7568f34288f221c5e4
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-02-28 06:57:00 -0300

    new branch

--- ternario.php
+++ ternario.php
@@ -1,5 +1,5 @@
 <?php
 
 $idade = 18;
-$mensagem = $idade < 18 ? 'Você é menor de idade' : 'Você é maior de idade';
+$mensagem = $idade < 18 ? 'Você é menor de idade não pode entrar' : 'Você é maior de idade, pode entrar';
 echo $mensagem;
