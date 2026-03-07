33b86718b63a5ae3d1b84309b8f6b965a97f9152
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-03-06 23:47:15 -0300

    inclusao do desafio Calculo do IMC

---
+++ estudos/desafios/imc.php
@@ -0,0 +1,32 @@
+<?php
+
+$peso = 100;
+$altura = 1.65;
+$imc = $peso / ($altura ** 2);
+
+echo "O IMC é: $imc" . PHP_EOL;
+
+if ($imc < 18.5) {
+   echo "Abaixo do peso" . PHP_EOL;
+   
+} elseif
+ ($imc >= 18.5 && $imc < 25){  
+    echo "Peso normal" . PHP_EOL;
+
+} elseif
+ ($imc >= 25 && $imc < 30) {                
+    echo "Sobrepeso" . PHP_EOL;
+
+} elseif
+ ($imc >= 30 && $imc < 35) {
+    echo "Obesidade grau 1" . PHP_EOL;
+    
+} elseif 
+  ($imc >= 35 && $imc < 40) {
+    echo "Obesidade grau 2" . PHP_EOL;
+
+} else {
+    echo "Obesidade grau 3" . PHP_EOL;
+}
+
+
