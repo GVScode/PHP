6da0d2ffcededd11a4a9e493a03d684fdc6879ed
Guilherme Varjao/ <guilhermmesantos98@gmail.com>
2026-03-27 17:06:13 -0300

    Inicio do projeto screen match

---
+++ Criando_aplicacao/screen-match.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Bem vindo (a) ao Screen Match!\n\n";
+
+$nomeFilme = "Top Gun - Maverick \n";
+$anoLancamento = 2022 . "\n";
+$notaFilme = 8.8 . "\n";
+$incluidoNoPlano = true;
+
+echo "nome do filme .....: " . $nomeFilme;
+echo "Ano de lançamento .: " . $anoLancamento;
+echo "Nota ..............: " . $notaFilme;
+echo "Incluído no plano  ? " . $incluidoNoPlano;
