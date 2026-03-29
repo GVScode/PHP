6c4bc35b85ef5337ddb4119c16b81ce1e65d48d9
Guilherme Varjão <guilhermmesantos98@gmail.com>
2026-03-29 14:23:47 -0300

    incluindo a função argv e fazendo um match de titulo e genero

---
+++ Criando_aplicacao/exercicios29_03/ex5.php
@@ -0,0 +1,8 @@
+<?php
+
+$temperaturaEmCelsius = 30.4; // Modifique esse valor para a temperatura que desejar
+$temperaturaEmFahrenheit = $temperaturaEmCelsius * 1.8 + 32;
+
+$mensagem = "A temperatura de $temperaturaEmCelsius Celsius é equivalente a $temperaturaEmFahrenheit Fahrenheit";
+
+echo $mensagem;
